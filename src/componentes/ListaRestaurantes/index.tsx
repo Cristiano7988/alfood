2c22f0d0c295d2777e3feaa2db82d140a44a0876
Cristiano7988 <cristianooliveiramorales@gmail.com>
2022-12-19 14:47:09 -0300

    Acrescentando ordenação

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -17,6 +17,7 @@ const ListaRestaurantes = () => {
   const [proximaPagina, setProximaPagina] = useState("");
   const [paginaAnterior, setPaginaAnterior] = useState("");
   const [busca, setBusca] = useState("");
+  const [ordenacao, setOrdenacao] = useState("");
 
   const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
     axios.get<IPaginacao<IRestaurante>>(url, opcoes)
@@ -40,6 +41,7 @@ const ListaRestaurantes = () => {
     }
 
     if (busca) opcoes.params.search = busca
+    if (ordenacao) opcoes.params.ordering = ordenacao
     carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
   }
 
@@ -50,8 +52,24 @@ const ListaRestaurantes = () => {
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     <form onSubmit={buscar}>
-      <TextField value={busca} onChange={evento => setBusca(evento.target.value)} />
-      <Button type="submit">Buscar</Button>
+      <div>
+        <TextField value={busca} onChange={evento => setBusca(evento.target.value)} />
+      </div>
+      <div>
+        <label htmlFor='select-ordenacao'>Ordenação</label>
+        <select
+          name="select-ordenacao"
+          value={ordenacao}
+          onChange={(e) => setOrdenacao(e.target.value)}
+        >
+          <option value="">Padrão</option>
+          <option value="id">Por id</option>
+          <option value="nome">Por nome</option>
+        </select>
+      </div>
+      <div>
+        <Button type="submit">Buscar</Button>
+      </div>
     </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={() => carregarDados(proximaPagina)}>
