c5be1d7ebd594d45ce6624b587f673063edda897
Cristiano7988 <cristianooliveiramorales@gmail.com>
2022-12-20 13:37:27 -0300

    Adicionando edição, criação e deleção dos pratos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
 import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPratos';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import Home from './paginas/Home';
@@ -17,8 +18,8 @@ function App() {
         <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
         <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
         <Route path="pratos" element={<AdministracaoPratos />} />
-        <Route path="pratos/novo" element={<></>} />
-        <Route path="pratos/:id" element={<></>} />
+        <Route path="pratos/novo" element={<FormularioPrato/>} />
+        <Route path="pratos/:id" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
